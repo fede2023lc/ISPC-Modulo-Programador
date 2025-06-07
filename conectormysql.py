25a6c21b8c475f051b4b78de0cc6804c136eca47
fede2023lc <fede2023lc@hotmail.com>
2025-06-07 00:38:57 -0300

    Update conectormysql.py

--- conectormysql.py
+++ conectormysql.py
@@ -1,11 +1,11 @@
 import mysql.connector
 
 def conectarDB():
-    conn = mysql.connector.connect(
+    conector = mysql.connector.connect(
         host="localhost",
-        user="root",    #COMPLETAR CON SU USER DE MYSQL
-        password="123456",  #COMPLETAR CON SU PASSWORD DE MYSQL
+        user="root",
+        password="123456",
         database="skyroute"
         )
-    cursor = conn.cursor()
-    return conn, cursor
+    cursor = conector.cursor()
+    return conector, cursor
